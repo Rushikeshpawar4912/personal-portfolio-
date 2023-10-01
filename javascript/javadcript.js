20a406bfbe6a2ca47bfb765c2774d907ceea07f4
Rushikeshpawar4912 <138990079+Rushikeshpawar4912@users.noreply.github.com>
2023-10-01 18:18:52 +0530

    files

---
+++ javascript/javadcript.js
@@ -0,0 +1,7 @@
+        // Function to refresh the page
+        function refreshPage() {
+            location.reload();
+        }
+
+        // Set the refresh interval to 5 seconds (5000 milliseconds)
+        setInterval(refreshPage, 10000);
